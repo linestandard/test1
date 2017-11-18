133f57ecb64e7dd57d98c9c5cbbd597aeb0b0c2a
Joseph Lin <tailrecurse@gmail.com>
2017-11-17 19:46:20 -0800

    Connect Mac UI to RssModel

--- RssFeedReader.Mac/RssFeedReader.Mac/AppDelegate.cs
+++ RssFeedReader.Mac/RssFeedReader.Mac/AppDelegate.cs
@@ -1,5 +1,6 @@
 ﻿using AppKit;
 using Foundation;
+using System;
 
 namespace RssFeedReader.Mac
 {
@@ -13,6 +14,12 @@ namespace RssFeedReader.Mac
         public override void DidFinishLaunching(NSNotification notification)
         {
             // Insert code here to initialize your application
+
+            // TODO: temporary start
+            var source = new Uri("http://feeds.feedburner.com/EtsBreakingNews");
+            var feed = RssModel.Feed.Read(source);
+            var x = feed.Items.Count;
+            // TODO: temporary end
         }
 
         public override void WillTerminate(NSNotification notification)
